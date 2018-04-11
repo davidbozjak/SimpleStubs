386d9801c04bd165a8635594f317ba2de0256408
David Bozjak <bojak@microsoft.com>
2018-04-11 08:10:31 +0200

    Fixed legacy class name

--- RuntimeComponentTests/StubRuntimeComponentTypesTest.cs
+++ RuntimeComponentTests/StubRuntimeComponentTypesTest.cs
@@ -6,7 +6,7 @@ using System;
 namespace RuntimeComponentTests
 {
     [TestClass]
-    public class UnitTest1
+    public class StubRuntimeComponentTypesTest
     {
         [TestMethod]
         public async Task StubbedIAsyncActionIsAwaitable()
